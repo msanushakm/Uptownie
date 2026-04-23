6f7fd8f514d011976dc4c59783f550bcbb81d74d
msanushakm <km037138@gmail.com>
2026-04-23 13:27:02 +0530

    Update ShirtCards.jsx

--- uptownie/src/components/adminPage/ShirtCards.jsx
+++ uptownie/src/components/adminPage/ShirtCards.jsx
@@ -10,7 +10,7 @@ function ShirtCards(){
     const navigate=useNavigate()
 
     useEffect(()=>{
-    axios.get("http://localhost:3001/products/category/shirt")
+    axios.get("https://uptownie.onrender.com/products/category/shirt")
     .then(res=>{
     setProducts(res.data)
     })
@@ -21,11 +21,11 @@ function ShirtCards(){
         const value=e.target.value;
         setSearchKey(value)
         if (value===""){
-            axios.get("http://localhost:3001/products/category/shirt")
+            axios.get("https://uptownie.onrender.com/products/category/shirt")
             .then(res => setProducts(res.data))
         }else{
             try {
-                const res=await axios.get("http://localhost:3001/products/search/shirt/"+value)
+                const res=await axios.get("https://uptownie.onrender.com/products/search/shirt/"+value)
                 setProducts(res.data)
             } catch (error) {
                 console.log(error)
@@ -48,7 +48,7 @@ function ShirtCards(){
         <div className="productContainer">
         {products.map((product)=>(        
         <div className="cardd" onClick={() => {navigate(`/productDetails/${product._id}`)}} style={{cursor:"pointer"}}>
-            <img src={`http://localhost:3001/upload/${product.image}`}/>
+            <img src={`https://uptownie.onrender.com/upload/${product.image}`}/>
             <h3>{product.name}</h3>
             <p>{product.description}</p>
             <h4>₹{product.price}</h4>
