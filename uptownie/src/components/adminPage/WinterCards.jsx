7f31800801cff4c613c26ffc0db98f322461bc35
msanushakm <km037138@gmail.com>
2026-04-23 13:32:29 +0530

    Update API endpoints to use production URL

--- uptownie/src/components/adminPage/WinterCards.jsx
+++ uptownie/src/components/adminPage/WinterCards.jsx
@@ -10,7 +10,7 @@ function WinterCards(){
     const navigate=useNavigate()
 
     useEffect(()=>{
-    axios.get("http://localhost:3001/products/category/winter_wear")
+    axios.get("https://uptownie.onrender.com/products/category/winter_wear")
     .then(res=>{
     setProducts(res.data)
     })
@@ -21,11 +21,11 @@ function WinterCards(){
         const value=e.target.value;
         setSearchKey(value)
         if (value===""){
-            axios.get("http://localhost:3001/products/category/winter_wear")
+            axios.get("https://uptownie.onrender.com/products/category/winter_wear")
             .then(res => setProducts(res.data))
         }else{
             try {
-                const res=await axios.get("http://localhost:3001/products/search/winter_wear/"+value)
+                const res=await axios.get("https://uptownie.onrender.com/products/search/winter_wear/"+value)
                 setProducts(res.data)
             } catch (error) {
                 console.log(error)
@@ -48,7 +48,7 @@ function WinterCards(){
         <div className="productContainer">
         {products.map((product)=>(        
         <div className="cardd" onClick={() => {navigate(`/productDetails/${product._id}`)}} style={{cursor:"pointer"}}>
-            <img src={`http://localhost:3001/upload/${product.image}`}/>
+            <img src={`https://uptownie.onrender.com/upload/${product.image}`}/>
             <h3>{product.name}</h3>
             <p>{product.description}</p>
             <h4>₹{product.price}</h4>
